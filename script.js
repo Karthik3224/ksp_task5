b1877ac6ed3689b230c6eb5d7bb490816dc97c02
Karthik3224 <153287084+Karthik3224@users.noreply.github.com>
2023-12-11 21:34:12 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+console.log ("hello guys");
+alert("welcome to website");
+// variable
+let box1=2;
+let box2=4;
+let box3=3;
+console.log (box3);
+let num = 10;
+num = 13;
+console.log (num);
+const age = 19;
+ let number = 30;//integr
+ let word = "bike";//string
+ let box = true;//boolean
+ //arithmetic operators
+ let num1 = 10;
+ let num2 = 20;
+ console.log (num1 + num2); 
+ console.log (num1 - num2);
+ console.log (num1 * num2);  
+ console.log (num1 / num2);
+ console.log (num1 % num2);
+ console.log (num1 ** num2);  
+ //assignment operators
+ let num3 = 5;
+ num3 += 5;
+ console.log (num3);
+ let num4 = 6;
+num4 -= 3;
+console.log (num4);
+let num5 = 9;
+num5 *= 3;
+console.log (num5);
+//comparision operators
+let num6 = 10;
+let num7 = 10;
+let num9 = "10";
+console.log (num8=num6);
+console.log (num6==num7);
+console.log (num6===num9);
+console.log (num6!=num7);
